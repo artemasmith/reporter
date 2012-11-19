4cc5a25d147b3912c92ceca99246aad4329f793f
root <root@fedoratemp.tmn.transneft.ru>
2012-11-19 10:58:10 +0600

    del wrong code

--- init.rb
+++ init.rb
@@ -1,10 +1,8 @@
 Redmine::Plugin.register :reporter do
   name 'Reporter plugin'
-<<<<<<< HEAD
-  author 'Author name'
-=======
+
   author 'Artem Kuznetsov'
->>>>>>> b74ca4e36ec4284b651635b1c10bc892613e5ba9
+
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
