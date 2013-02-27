fd37c9e3cc004a3ca44bc67ac59fae3e51424a7f
Artem Kuznetsov <artemasmith@gmail.com>
2013-02-27 16:50:34 +0600

    bug fixed

--- app/models/repissue.rb
+++ app/models/repissue.rb
@@ -40,7 +40,19 @@ def self.check_to_save(mode,period,end_date)
 end
 
 def self.get_start_date(period)
-	start_day=(Date.today-period).beginning_of_month
+    case period
+	when 7
+	    start_day=(Date.today).beginning_of_week
+	when 30
+	    start_day=(Date.today).beginning_of_month
+	when 90
+	    start_day=(Date.today).beginning_of_quarter
+	when 365
+	    start_day=(Date.today).beginning_of_year
+	when 1000
+	    start_day='2012-01-01'.to_date #(Date.today-period).beginning_of_year
+    end
+    return start_day
 end
 
 #return all exxeded and soon ended issues
@@ -50,11 +62,14 @@ end
 #all-we return all tasks
 def self.all(mode, period = 7)
     
-    if mode=="soon"
-        issues=Issue.find(:all, :conditions => ["DATEDIFF(CURDATE(), due_date)<=3 and (status_id < 9 or status_id > 11)"])
-    else
-	start_date=get_start_date(period)
-	issues=Issue.find(:all, :conditions => ["DATEDIFF(due_date," + start_date.to_s + " )<="+period.to_s+" and (status_id < 9 or status_id > 11)"])
+    issues=[]
+    
+    case mode
+	when "soon"
+	    issues=Issue.find(:all, :conditions => ["DATEDIFF(due_date,CURDATE())>0 and  DATEDIFF(due_date, CURDATE())<=3 and (status_id < 9 or status_id > 11)"])
+	when "exceed"
+	    start_date=get_start_date(period)
+	    issues=Issue.find(:all, :conditions => ["DATEDIFF(due_date,\'" + start_date.to_s + "\' )<="+period.to_s+" and DATEDIFF(due_date,\'"+ start_date.to_s + " \')>0 and (status_id < 9 or status_id > 11)"])
     end
 
     @resissues=[]
@@ -69,7 +84,7 @@ def self.all(mode, period = 7)
         temp[:project_id]= Project.find_by_id(issue[:project_id]).to_s + '-' + issue[:project_id].to_s
         temp[:done_ratio]=issue[:done_ratio]
         temp[:start_date]=issue[:start_date]
-        temp[:deadend] = issue[:due_date]  #s[0].to_s
+        temp[:deadend] = issue[:due_date]
         temp[:delayed_days]=delta.to_i.abs
         @resissues[j]=temp
         j+=1
