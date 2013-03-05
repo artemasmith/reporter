e3a888fe3a73df97d60e93e6535fe36ae51aac85
Artem Kuznetsov <artemasmith@gmail.com>
2013-03-05 10:27:40 +0600

    normal menu link added. Now vesion 0.1.0

--- init.rb
+++ init.rb
@@ -4,7 +4,9 @@ Redmine::Plugin.register :reporter do
   author 'Artem Kuznetsov'
 
   description 'This is the reporting-tools plugin for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/artemasmith/reporting-tools.git'
-  author_url 'https://github.com/artemasmith/reporting-tools.git'
+  author_url 'https://github.com/artemasmith/reporting-tools.git'	
+  
+  menu :admin_menu, :reports, { :controller => 'repissues', :action=> 'index'}, :last => true
 end
